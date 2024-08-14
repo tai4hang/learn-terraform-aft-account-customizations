afe347e5d8190741ac21f06c41435ab43a3f1808
tai4hang <38436336+tai4hang@users.noreply.github.com>
2024-08-14 16:37:49 -0400

    Update s3.tf

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,8 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "sandbox_bucket_new" {
+  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}-test"
+  acl    = "private"
+}
